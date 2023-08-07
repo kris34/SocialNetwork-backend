30e73cdfabb9b57393d5c1dffdab792a8c080f55
Kristian Lalov <lalov.kr@gmail.com>
2023-08-07 23:50:13 +0300

    added user model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,21 @@
 const { model, Types, Schema } = require('mongoose');
 
 const userSchema = new Schema({
-    
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    minLength: [3, 'Username has to be at least 3 chararakter long!'],
+    maxLength: [15, 'Username cannot be longer then 15 charakters.'],
+  },
+  email: { type: String, required: true, unique: true },
+  password: {
+    type: String,
+    required: true,
+    minLength: [5, 'Password has to be at least 5 charakters long.'],
+  },
 });
+
+const User = new model('User', userSchema);
+
+module.exports = User;
